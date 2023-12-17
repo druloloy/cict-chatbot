d7850f028a602f37364c0ea2ac204744fea60eb3
Andrew Loloy <41189336+druloloy@users.noreply.github.com>
2023-12-17 22:24:51 +0800

    updated max_tokens

--- chat-engine/core/prompt.js
+++ chat-engine/core/prompt.js
@@ -3,57 +3,53 @@
 // } = require('../utils/openai');
 
 function createResponse(query, responses, openai) {
-    const {
-        instruction,
-        question,
-        context,
-        fallback,
-        keyNotes
-    } = getTemplate(query, responses);
+  const { instruction, question, context, fallback, keyNotes } = getTemplate(
+    query,
+    responses
+  );
 
-    const options = {
-        messages: [
-            {
-                role: 'system',
-                content: context
-            },
-            {
-                role: 'system',
-                content: fallback
-            },
-            {
-                role: 'system',
-                content: keyNotes
-            },
-            {
-                role: 'system',
-                content: instruction
-            },
-            {
-                role: 'user',
-                content: question
-            }
-        ],
-        temperature: 0.9,
-        model: 'gpt-3.5-turbo',
-        top_p: 1,
-        max_tokens: 400,
-        n: 1
-    };
+  const options = {
+    messages: [
+      {
+        role: "system",
+        content: context,
+      },
+      {
+        role: "system",
+        content: fallback,
+      },
+      {
+        role: "system",
+        content: keyNotes,
+      },
+      {
+        role: "system",
+        content: instruction,
+      },
+      {
+        role: "user",
+        content: question,
+      },
+    ],
+    temperature: 0.9,
+    model: "gpt-3.5-turbo",
+    top_p: 1,
+    max_tokens: 600,
+    n: 1,
+  };
 
-    return new Promise(async (resolve, reject) => {
-        try {
-            const response = await openai.chat.completions.create(options);
-            resolve(response.choices[0].message.content);
-            
-        } catch (error) {
-            reject(error);
-        }
-    });
+  return new Promise(async (resolve, reject) => {
+    try {
+      const response = await openai.chat.completions.create(options);
+      resolve(response.choices[0].message.content);
+    } catch (error) {
+      reject(error);
+    }
+  });
 }
 
 function getTemplate(query, responses) {
-    const fallbackMessage = `I'm sorry, but I don't have information on that topic. Please feel free to ask me something else, and I'll do my best to help.<br><br>
+  const fallbackMessage = `I'm sorry, but I don't have information on that topic. Please feel free to ask me something else, and I'll do my best to help.<br><br>
                             For more information you may reach the TCU's Registrar Office in the following contact details: <br> 
                             
                             - Facebook: [@officialtcuregistrar](www.facebook.com/officialtcuregistrar/)
@@ -62,9 +58,8 @@ function getTemplate(query, responses) {
                             - Email: [tcuregistrarofficial@gmail.com](mailto:tcuregistrarofficial@gmail.com)
                             - Student verification email: [tcuregistrarcav@gmail.com](mailto:tcuregistrarcav@gmail.com)
                             `;
-    // create promps
-    const instruction =
-        `
+  // create promps
+  const instruction = `
         You are an AI assistant named Blitzle. As an AI assistant, your primary goal is to provide helpful and relevant information within the given context. You are clever, knowledgeable, and friendly, but you should always stay on-topic and avoid going off on tangents.
 
         - Your responses should be directly related to the context provided.
@@ -79,22 +74,22 @@ function getTemplate(query, responses) {
         Use the key notes as additional guidelines in crafting your response to ensure that it aligns with the context.
     `;
 
-    const question = `
+  const question = `
     This is the question you need to answer. If the question is inapproppriate, personal, or doesn't relate to any of the context rovidfed then send only the fallback message and ONLY that.
     Question: ${query}
     `;
 
-    const context = `
+  const context = `
     You will rely on the context provided to answer the question.
-    Context/Reference: ${responses.join('\n')}
+    Context/Reference: ${responses.join("\n")}
     `;
 
-    const fallback = `
+  const fallback = `
     If the answer is far from the context, you can answer it with ONLY the fallback message and most importantly, you shouldn't give false information.
     Fallback: ${fallbackMessage}
     `;
 
-    const keyNotes = `
+  const keyNotes = `
     These are the key notes you need to consider in crafting your response as an AI assistant.
     - To apply, visit the TCU Facebook page for admission dates, submit documents to the Registrar, take an entrance exam, and bring required documents for enrollment as per Facebook announcements.
     - Enrollment requirements vary by applicant type (freshmen, ALS passers, Balik-Aral).
@@ -109,16 +104,15 @@ function getTemplate(query, responses) {
     - Online admission slots are available; check the TCU Facebook page for updates.
     `;
 
-
-    return {
-        instruction,
-        question,
-        context,
-        fallback,
-        keyNotes
-    };
+  return {
+    instruction,
+    question,
+    context,
+    fallback,
+    keyNotes,
+  };
 }
 
 module.exports = {
-    createResponse
+  createResponse,
 };
